d361a848946dec585635aa2ee09f794316433931
Mariana Piz <marianapiz0412@gmail.com>
2024-06-08 11:48:46 +0300

    Initial commit

---
+++ RecipeFinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RecipeFinder
+//
+//  Created by Mariana Piz on 08.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
